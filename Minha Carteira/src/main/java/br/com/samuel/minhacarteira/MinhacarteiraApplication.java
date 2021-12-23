a877994f8767e883962fc6f9aa39313fe1078256
samuel freire <samuellfreire@gmail.com>
2021-12-23 16:06:42 -0300

    Commit Project Spring Boot

---
+++ Minha Carteira/src/main/java/br/com/samuel/minhacarteira/MinhacarteiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.samuel.minhacarteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhacarteiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhacarteiraApplication.class, args);
+	}
+
+}
---
+++ Minha Carteira/src/test/java/br/com/samuel/minhacarteira/MinhacarteiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samuel.minhacarteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhacarteiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
